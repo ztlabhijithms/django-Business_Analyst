ad1d4022fc6ff6dd7c4cee95cec66ca90dda10f2
ABHIJITH54 <jabhijith52@gmail.com>
2026-03-04 22:53:32 +0530

    KHIH

--- BusinessAnalyst/BusinessAnalyst/asgi.py
+++ BusinessAnalyst/BusinessAnalyst/asgi.py
@@ -17,6 +17,7 @@ os.environ.setdefault("DJANGO_SETTINGS_MODULE", "BusinessAnalyst.settings")
 django_asgi_app = get_asgi_application()
 
 application = ProtocolTypeRouter({
+    
     "http": django_asgi_app,
     "websocket": AuthMiddlewareStack(
         
